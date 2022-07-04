dd30307c42e8b1caa5ab236f353ea424b487dbe5
ami-aman <91549653+ami-aman@users.noreply.github.com>
2022-07-04 17:09:20 +0530

    install CIOAPN using podspec not tag

--- ios/Notification Service/NotificationService.swift
+++ ios/Notification Service/NotificationService.swift
@@ -1,11 +1,13 @@
 import UserNotifications
-
+import CioMessagingPush
+import CioTracking
 class NotificationService: UNNotificationServiceExtension {
    
       override func didReceive(_ request: UNNotificationRequest,
                                withContentHandler contentHandler:
                                @escaping (UNNotificationContent) -> Void) {
         
+        MessagingPush.shared.didReceive(request, withContentHandler: contentHandler)
 
       }
   
