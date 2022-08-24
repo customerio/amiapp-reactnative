9d0f731ed591dd572678a0ed12ff14c878fae283
ami-aman <91549653+ami-aman@users.noreply.github.com>
2022-08-24 20:08:23 +0400

    husky hook pre-commit test

--- components/ClearIdentity.js
+++ components/ClearIdentity.js
@@ -2,51 +2,50 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import FeatureButton from "./common/FeatureButton";
 import { SubHeaderText } from "./common/Text";
-import { CustomerIO } from 'customerio-reactnative';
-
+import { CustomerIO } from "customerio-reactnative";
 
 const ClearIdentity = (props) => {
+  const ClearUserIdentity = () => {
+    // MARK:- CLEAR USER IDENTITY
+    CustomerIO.clearIdentify();
 
-    const ClearUserIdentity = () => {
-        // MARK:- CLEAR USER IDENTITY
-        CustomerIO.clearIdentify()
-        
-        props.onClear()
-    }
+    props.onClear();
+  };
 
-    return (
-        <View style={styles.container}>
-            <SubHeaderText label = "CLEAR USER IDENTITY"/>
-            <View style={styles.innerContainer}>
-                <FeatureButton
-                title ="Clear user identity"
-                style={{marginBottom: 20}}
-                onPress = {() => ClearUserIdentity()}></FeatureButton>
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <SubHeaderText label="CLEAR USER IDENTITY" />
+      <View style={styles.innerContainer}>
+        <FeatureButton
+          title="Clear user identity of the user"
+          style={{ marginBottom: 20 }}
+          onPress={() => ClearUserIdentity()}
+        ></FeatureButton>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container : {
-        backgroundColor: 'white',
-        marginTop: 20,
-        borderRadius: 25
-    },
-    innerContainer: {
-        marginTop: 5,
-    },
-    input: {
-        height: 40,
-        marginLeft: 12,
-        marginRight: 12,
-        marginBottom: 12,
-        borderWidth: 1,
-        padding: 10,
-        backgroundColor: 'white',
-        borderColor: '#e6e6e6',
-        borderRadius: 10
-      },
-})
+  container: {
+    backgroundColor: "white",
+    marginTop: 20,
+    borderRadius: 25,
+  },
+  innerContainer: {
+    marginTop: 5,
+  },
+  input: {
+    height: 40,
+    marginLeft: 12,
+    marginRight: 12,
+    marginBottom: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "white",
+    borderColor: "#e6e6e6",
+    borderRadius: 10,
+  },
+});
 
 export default ClearIdentity;
