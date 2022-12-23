aab5d65a429100cfc62a418fece086e554d6288d
ami-aman <91549653+ami-aman@users.noreply.github.com>
2022-12-23 18:38:44 +0530

    v2 updation in notification extension

--- ios/Notification Service/NotificationService.swift
+++ ios/Notification Service/NotificationService.swift
@@ -1,13 +1,18 @@
 import UserNotifications
 import CioMessagingPush
+import CioTracking
+
 class NotificationService: UNNotificationServiceExtension {
    
       override func didReceive(_ request: UNNotificationRequest,
                                withContentHandler contentHandler:
                                @escaping (UNNotificationContent) -> Void) {
         
+        CustomerIO.initialize(siteId: "94541e1bbff594682089", apiKey: "914ea21ebfa87bf771f4", region: .US) { config in
+          config.autoTrackDeviceAttributes = true
+          config.logLevel = .info
+        }
         MessagingPush.shared.didReceive(request, withContentHandler: contentHandler)
-
       }
   
       override func serviceExtensionTimeWillExpire() {
