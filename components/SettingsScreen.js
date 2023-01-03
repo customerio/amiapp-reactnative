9dafd262eff95325ae48f444007816fbf30434bc
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-01-03 15:44:01 +0530

    settings screen

--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -1,8 +1,9 @@
-import React, {useLayoutEffect} from 'react'
-import { View, StyleSheet, Text} from 'react-native';
+import React, {useLayoutEffect, useState} from 'react'
+import { View, StyleSheet, Text, Image} from 'react-native';
+import { TextInput, TouchableOpacity} from 'react-native-gesture-handler';
 
 const SettingsScreen = ({navigation}) => {
-
+const [deviceToken, setDeviceToken] = useState('')
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShadowVisible: false,
@@ -11,10 +12,125 @@ const SettingsScreen = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-       <View style={{flex:1,backgroundColor:'white', paddingTop: 50}}>
-              <View style={{justifyContent:'space-around'}}>
-                  <Text> Settings</Text>
+       <View style={styles.innerContainer}>
+              <View style={styles.headerView}>
+                  <Text style={styles.textHeaderLabel}> Settings</Text>
             </View>
+            <View style={[styles.rowView, {marginTop: 10}]}>
+                <View style={styles.stackColumnView}>
+                  <Text style={styles.textLabel}>Device Token</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={deviceToken}
+                    placeholder="device token"
+                    editable={false}
+                  />
+                  <TouchableOpacity
+                  style={styles.copyToClipboardButton}
+                      onPress={() => settingsTapped()}>
+                          <Image 
+                          style={styles.copyToClipboardImage}
+                          source={require('../assets/images/paper.png')}>
+
+                          </Image>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            {/* CIO Track URL */}
+              <View style={styles.rowView}>
+                <View style={styles.stackColumnView}>
+                  <Text style={styles.textLabel}>CIO Track URL</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={deviceToken}
+                    placeholder="track.customer.io"
+                    editable={false}
+                />
+                <View style={styles.copyToClipboardButton}></View>
+                </View>
+              </View>
+
+            {/* Gist Environment */}
+              <View style={styles.rowView}>
+                <View style={styles.stackColumnView}>
+                  <Text style={styles.textLabel}>Gist Environment</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={deviceToken}
+                    placeholder="Gist"
+                    editable={false}
+                />
+                <View style={styles.copyToClipboardButton}></View>
+                </View>
+            </View>
+
+
+            {/* Section #2 */}
+            <View style={[styles.rowView, {marginTop: 30}]}>
+                <View style={styles.stackColumnView}>
+                  <Text style={styles.textLabel}>Site Id</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={deviceToken}
+                    placeholder="siteId"
+                    editable={false}
+                  />
+                  <View style={styles.copyToClipboardButton}></View>
+                </View>
+              </View>
+              {/* API Key */}
+              <View style={styles.rowView}>
+                <View style={styles.stackColumnView}>
+                  <Text style={styles.textLabel}>API Key</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={deviceToken}
+                    placeholder="Api key"
+                    editable={false}
+                  />
+                  <View style={styles.copyToClipboardButton}></View>
+                </View>
+              </View>
+              {/* Org Id */}
+              <View style={styles.rowView}>
+                <View style={styles.stackColumnView}>
+                  <Text style={styles.textLabel}>Org Id</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={deviceToken}
+                    placeholder="Gist Org id"
+                    editable={false}
+                  />
+                  <View style={styles.copyToClipboardButton}></View>
+                </View>
+              </View>
+
+              {/* Section #3 */}
+            <View style={[styles.rowView, {marginTop: 30}]}>
+                <View style={styles.stackColumnView}>
+                  <Text style={styles.textLabel}>backgroundQueueSecondsDelay</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={deviceToken}
+                    placeholder="number"
+                    editable={false}
+                  />
+                  <View style={styles.copyToClipboardButton}></View>
+                </View>
+              </View>
+              {/* Org Id */}
+              <View style={styles.rowView}>
+                <View style={styles.stackColumnView}>
+                  <Text style={styles.textLabel}>backgroundQueueMinNumberOfTasks</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={deviceToken}
+                    placeholder="number"
+                    editable={false}
+                  />
+                  <View style={styles.copyToClipboardButton}></View>
+                </View>
+              </View>
         </View> 
     </View>
   )
@@ -22,12 +138,62 @@ const SettingsScreen = ({navigation}) => {
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        backgroundColor: "#fff"
+        backgroundColor: "white",
     },
-    textLabel: {
-        padding: 20,
-        color: '#fff',
-        lineHeight: 25
-    }
+    sectionView: {
+      flexDirection: 'row',
+      marginTop: 30
+    },
+    copyToClipboardButton:{
+      alignContent: 'flex-end',
+      justifyContent: 'flex-end',
+      alignSelf:'flex-end',
+      height: 30,
+      width: 25
+    },
+    copyToClipboardImage: {
+      width: 20,
+      height: 20,
+  },
+    rowView:{
+      flexDirection: 'row',
+      paddingTop: 10
+    },
+    stackColumnView: {
+      flexDirection: 'row',
+      justifyContent:'space-between',
+      alignItems: 'stretch',
+      alignSelf: 'stretch',
+      width: '100%',
+    },
+    textLabel:{
+      fontSize: 14,
+      alignContent: 'flex-end',
+      justifyContent: 'flex-end',
+      alignSelf:'flex-end',
+      color: "#404040"
+    },
+    headerView:{
+      justifyContent:'space-around',
+    },
+    textHeaderLabel: {
+        color: '#000',
+        fontSize: 18,
+        fontWeight: 'bold'
+      },
+    innerContainer:{
+      flex:1,
+      backgroundColor:'white',
+      padding: 10
+    },
+    input: {
+      width: '60%',
+      borderWidth: 1,
+      padding:  5,
+      backgroundColor: 'white',
+      borderColor: '#fff',
+      borderBottomColor: '#e6e6e6',
+      borderRadius: 10,
+    },
 })
 export default SettingsScreen;
