8d4a35313fadb5949864de6152b0c1d230424734
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-01-10 15:42:26 +0530

    get device token in react native app

--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -2,6 +2,9 @@ import React, {useLayoutEffect, useState} from 'react'
 import { View, StyleSheet, Text, Image, Switch} from 'react-native';
 import { ScrollView, TextInput, TouchableOpacity} from 'react-native-gesture-handler';
 import Env from '../env';
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
+
 
 const SettingsScreen = ({navigation}) => {
 const [deviceToken, setDeviceToken] = useState('')
@@ -34,6 +37,57 @@ useLayoutEffect(() => {
         break
     }
   }
+
+  PushNotification.configure({
+    // (optional) Called when Token is generated (iOS and Android)
+    onRegister: function (token) {
+      console.log("TOKEN:", token);
+    },
+  
+    // // (required) Called when a remote is received or opened, or local notification is opened
+    // onNotification: function (notification) {
+    //   console.log("NOTIFICATION:", notification);
+  
+    //   // process the notification
+  
+    //   // (required) Called when a remote is received or opened, or local notification is opened
+    //   notification.finish(PushNotificationIOS.FetchResult.NoData);
+    // },
+  
+    // // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+    // onAction: function (notification) {
+    //   console.log("ACTION:", notification.action);
+    //   console.log("NOTIFICATION:", notification);
+  
+    //   // process the action
+    // },
+  
+    // // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+    // onRegistrationError: function(err) {
+    //   console.error(err.message, err);
+    // },
+  
+    // // IOS ONLY (optional): default: all - Permissions to register.
+    // permissions: {
+    //   alert: true,
+    //   badge: true,
+    //   sound: true,
+    // },
+  
+    // // Should the initial notification be popped automatically
+    // // default: true
+    // popInitialNotification: true,
+  
+    // /**
+    //  * (optional) default: true
+    //  * - Specified if permissions (ios) and token (android and ios) will requested or not,
+    //  * - if not, you must call PushNotificationsHandler.requestPermissions() later
+    //  * - if you are not using remote notification or do not have Firebase installed, use this:
+    //  *     requestPermissions: Platform.OS === 'ios'
+    //  */
+    // requestPermissions: true,
+  });
+  
   return (
     <ScrollView style={styles.container}>
        <View style={styles.innerContainer}>
