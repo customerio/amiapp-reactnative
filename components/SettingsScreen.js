1348261befd1196908e6c9a549a0e19150f360d7
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-01-10 17:16:08 +0530

    cleaned settings screen code

--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -8,6 +8,7 @@ import PushNotification from "react-native-push-notification";
 
 const SettingsScreen = ({navigation}) => {
 const [deviceToken, setDeviceToken] = useState('')
+const [trackUrl, setTrackUrl] = useState('')
 const [isDebugModeEnabled, setIsDebugModeEnabled] = useState(true)
 const [isPushEnabled, setIsPushEnabled] = useState(true)
 const [isTrackDeviceAttributesEnabled, setIsTrackDeviceAttributesEnabled] = useState(true)
@@ -42,50 +43,7 @@ useLayoutEffect(() => {
     // (optional) Called when Token is generated (iOS and Android)
     onRegister: function (token) {
       setDeviceToken(token["token"])
-    },
-  
-    // // (required) Called when a remote is received or opened, or local notification is opened
-    // onNotification: function (notification) {
-    //   console.log("NOTIFICATION:", notification);
-  
-    //   // process the notification
-  
-    //   // (required) Called when a remote is received or opened, or local notification is opened
-    //   notification.finish(PushNotificationIOS.FetchResult.NoData);
-    // },
-  
-    // // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-    // onAction: function (notification) {
-    //   console.log("ACTION:", notification.action);
-    //   console.log("NOTIFICATION:", notification);
-  
-    //   // process the action
-    // },
-  
-    // // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-    // onRegistrationError: function(err) {
-    //   console.error(err.message, err);
-    // },
-  
-    // // IOS ONLY (optional): default: all - Permissions to register.
-    // permissions: {
-    //   alert: true,
-    //   badge: true,
-    //   sound: true,
-    // },
-  
-    // // Should the initial notification be popped automatically
-    // // default: true
-    // popInitialNotification: true,
-  
-    // /**
-    //  * (optional) default: true
-    //  * - Specified if permissions (ios) and token (android and ios) will requested or not,
-    //  * - if not, you must call PushNotificationsHandler.requestPermissions() later
-    //  * - if you are not using remote notification or do not have Firebase installed, use this:
-    //  *     requestPermissions: Platform.OS === 'ios'
-    //  */
-    // requestPermissions: true,
+    }
   });
   return (
     <ScrollView style={styles.container}>
@@ -119,7 +77,7 @@ useLayoutEffect(() => {
                   <Text style={styles.textLabel}>CIO Track URL</Text>
                   <TextInput
                     style={styles.input}
-                    value={deviceToken}
+                    value={trackUrl}
                     placeholder="track.customer.io"
                     editable={false}
                 />
@@ -133,7 +91,7 @@ useLayoutEffect(() => {
                   <Text style={styles.textLabel}>Gist Environment</Text>
                   <TextInput
                     style={styles.input}
-                    value={deviceToken}
+                    // value={deviceToken}
                     placeholder="Gist"
                     editable={false}
                 />
